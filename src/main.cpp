021ccd6bec94b9fbc2a23379bc42bf6c1ee1c0f3
Max <7087907+maxpromer@users.noreply.github.com>
2024-06-03 18:07:54 +0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <TFT_eSPI.h>
 
 #define NET_PIN 0
 
@@ -6,6 +8,21 @@
 #define O2_PIN 14
 #define O3_PIN 12
 #define O4_PIN 13
+
+struct {
+  uint32_t output : 1;
+  struct {
+    uint32_t D1 : 1;
+    uint32_t D2 : 1;
+    uint32_t D3 : 1;
+    uint32_t A1 : 1;
+    uint32_t A2 : 1;
+  } input;
+  uint32_t rs485 : 1;
+  uint32_t wifi : 1;
+  uint32_t display : 1;
+  uint32_t i2c_and_rtc : 1;
+} qc;
 
 void TestOutput() {
   xTaskCreate([](void*) {
@@ -67,11 +84,26 @@ void TestInput() {
     pinMode(A2_PIN, INPUT);
 
     while(1) {
-      Serial.printf("%3d %3d %3d %6.02f %6.02f\n",
+      /*Serial.printf("%3d %3d %3d %6.02f %6.02f\n",
         digitalRead(D1_PIN), digitalRead(D2_PIN), digitalRead(D3_PIN),
         analogRead(A1_PIN) / 4095.0 * 9.9, analogRead(A2_PIN) / 4095.0 * 9.9
-      );
-      delay(300);
+      );*/
+      if (digitalRead(D1_PIN) == 0) {
+        qc.input.D1 = 1;
+      }
+      if (digitalRead(D2_PIN) == 0) {
+        qc.input.D2 = 1;
+      }
+      if (digitalRead(D3_PIN) == 0) {
+        qc.input.D3 = 1;
+      }
+      if (analogRead(A1_PIN) == 0) {
+        qc.input.A1 = 1;
+      }
+      if (analogRead(A2_PIN) == 0) {
+        qc.input.A2 = 1;
+      }
+      delay(100);
     }
 
     vTaskDelete(NULL);
@@ -129,14 +161,15 @@ void readXY_MD02() {
   if (Serial2.find("\x02\x04")) {
     uint8_t n = Serial2.read();
     if (n != 4) {
-      Serial.printf("Error data size : %d\n", n);
+      // Serial.printf("Error data size : %d\n", n);
       return;
     }
 
     temp = ((uint16_t)(Serial2.read() << 8) | Serial2.read()) / 10.0;
     humi = ((uint16_t)(Serial2.read() << 8) | Serial2.read()) / 10.0;
+    qc.rs485 = 1;
   } else {
-    Serial.println("ERROR Timeout");
+    // Serial.println("ERROR Timeout");
     return;
   }
 }
@@ -151,13 +184,64 @@ void TestModbus() {
 
     while(1) {
       readXY_MD02();
-      Serial.printf("XY-MD02: %.01f *C\t%.01f %%RH\n", temp, humi);
+      // Serial.printf("XY-MD02: %.01f *C\t%.01f %%RH\n", temp, humi);
       delay(2000);
     }
 
     vTaskDelete(NULL);
   }, "TestModbus", 5 * 1024, NULL, 10, NULL);
 }
+
+void TestWiFi() {
+  xTaskCreate([](void*) {
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    while(1) {
+      //Serial.println("Scan start");
+      int n = WiFi.scanNetworks();
+      if (n == 0) {
+          //Serial.println("no networks found");
+      } else {
+        qc.wifi = 1;
+          //Serial.print(n);
+          //Serial.println(" networks found");
+      }
+      delay(2000);
+    }
+
+    vTaskDelete(NULL);
+  }, "TestWiFi", 5 * 1024, NULL, 10, NULL);
+}
+
+TFT_eSPI tft = TFT_eSPI();
+#define LCD_BL_PIN 32
+
+void TestDisplay() {
+  xTaskCreate([](void*) {
+    tft.init();
+    pinMode(LCD_BL_PIN, OUTPUT);
+    digitalWrite(LCD_BL_PIN, LOW);
+
+    while(1) {
+      qc.display = 1;
+      tft.fillScreen(TFT_BLACK);
+      delay(500);
+      tft.fillScreen(TFT_RED);
+      delay(500);
+      tft.fillScreen(TFT_GREEN);
+      delay(500);
+      tft.fillScreen(TFT_BLUE);
+      delay(500);
+      tft.fillScreen(TFT_WHITE);
+      delay(500);
+    }
+
+    vTaskDelete(NULL);
+  }, "TestDisplay", 5 * 1024, NULL, 10, NULL);
+}
+
 
 #include <RTC.h>
 #include <Wire.h>
@@ -167,7 +251,7 @@ void TestI2CAndRTC() {
   xTaskCreate([](void*) {
     Wire.begin();
     while (!RTC_init()) {
-      Serial.println("Init RTC fail !!!");
+      //Serial.println("Init RTC fail !!!");
       delay(500);
     }
     
@@ -181,22 +265,24 @@ void TestI2CAndRTC() {
     };
 
     while (!RTC_write(&timeinfo_write)) {
-      Serial.println("RTC write fail !!!");
+      //Serial.println("RTC write fail !!!");
       delay(500);
     }
-    Serial.println("RTC writed.");
+    //Serial.println("RTC writed.");
 
     while(1) {
       struct tm timeinfo_read = { 0 };
       if (!RTC_read(&timeinfo_read)) {
-        Serial.println("RTC read fail !!!");
+        ///Serial.println("RTC read fail !!!");
         delay(500);
         continue;
       }
-      Serial.printf("RTC: %d/%d/%d %02d:%02d:%02d\n",
+      /*Serial.printf("RTC: %d/%d/%d %02d:%02d:%02d\n",
         timeinfo_read.tm_mday, timeinfo_read.tm_mon, timeinfo_read.tm_year + 1900,
         timeinfo_read.tm_hour, timeinfo_read.tm_min, timeinfo_read.tm_sec);
-      delay(1000);
+      */
+     qc.i2c_and_rtc = 1;
+     delay(1000);
     }
 
     vTaskDelete(NULL);
@@ -206,13 +292,20 @@ void TestI2CAndRTC() {
 void setup() {
   Serial.begin(115200);
 
+  memset(&qc, 0, sizeof(qc));
   TestOutput();
   TestInput();
   TestModbus();
   TestI2CAndRTC();
+  TestDisplay();
+  TestWiFi();
 }
 
 void loop() {
-  delay(1000);
+  Serial.printf(
+    "D1: %d\tD2: %d\tD3: %d\tA1: %d\tA2: %d\tRS485: %d\tWiFi: %d\tRTC-I2C: %d\n",
+    qc.input.D1, qc.input.D2, qc.input.D3, qc.input.A1, qc.input.A2, qc.rs485, qc.wifi, qc.i2c_and_rtc
+  );
+  delay(500);
 }
 
